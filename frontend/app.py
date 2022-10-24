67ec43888470b55f0b4b9108f34196fb6c8f4b2a
Soumil Shah <s.shah@jobtarget.com>
2022-10-24 08:30:19 -0400

    added improvments

--- frontend/app.py
+++ frontend/app.py
@@ -25,27 +25,48 @@ except Exception as e:
 
 app = Flask(__name__)
 
+global model
 model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 
 
 class Tokenizer(object):
-    def __init__(self):
-        self.model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 
-    def get_token(self, documents):
+    @staticmethod
+    def get_token(documents):
         sentences = [documents]
-        sentence_embeddings = self.model.encode(sentences)
+        sentence_embeddings = model.encode(sentences)
         encod_np_array = np.array(sentence_embeddings)
         encod_list = encod_np_array.tolist()
         return encod_list[0]
 
 
-class Search:
-    def __init__(self, vector):
-        self.vector = vector
+def create_scroll(raw_response):
+    try:
+        data = raw_response.get("hits", None).get("hits", None)
+        if not data: return None
+        data = data[-1]
+        score = data.get("_score", 1)
+        scroll_id_ = data.get("_id", None)
+        unique_scroll_id = "{},{}".format(score, scroll_id_)
+        return unique_scroll_id
 
-    def get(self):
-        query = {
+    except Exception as e:
+        return ""
+
+
+class Search(object):
+    def __init__(self, user_query):
+        self.user_query = user_query
+        self.vector = Tokenizer.get_token(documents=self.user_query)
+
+        self.es = Elasticsearch(hosts=[os.getenv("ELK_ENDPOINT")],
+                                http_auth=(os.getenv("ELK_USERNAME"),
+                                           os.getenv("ELK_PASSWORD")
+                                           )
+                                )
+
+    def get_query(self):
+        return {
             "query": {
                 "script_score": {
                     "query": {
@@ -61,18 +82,18 @@ class Search:
             }
         }
 
-        es = Elasticsearch(hosts=[os.getenv("ELK_ENDPOINT")],
-                           http_auth=(os.getenv("ELK_USERNAME"),
-                                      os.getenv("ELK_PASSWORD")
-                                      )
-                           )
+    def search(self, size=20, scroll_id=None):
+        if scroll_id is None:
+            res = self.es.search(index=os.getenv("ELK_INDEX"),
+                                 size=size,
+                                 body=self.get_query(),
+                                 request_timeout=55)
 
-        res = es.search(index=os.getenv("ELK_INDEX"),
-                        size=20,
-                        body=query,
-                        request_timeout=55)
+            scroll_id = create_scroll(res)
 
-        return res
+            return res, scroll_id
+        else:
+            pass
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -89,16 +110,19 @@ def ingest():
 def get_results_data():
     try:
         request_data = dict(request.form)
-        param = json.loads(request_data.get("data"))
-        user_inputs = param.get("user_inputs")
+        user_inputs = json.loads(request_data.get("data")).get("user_inputs")
 
-        token_instance = Tokenizer()
-        vector = token_instance.get_token(user_inputs)
+        search_helper = Search(user_query=user_inputs)
+        json_data, scroll_id = search_helper.search()
 
-        search_helper = Search(vector=vector)
-        data = search_helper.get()
+        data_cards = []
 
-        data_cards = [x['_source'] for x in data['hits']['hits']]
+        for x in json_data['hits']['hits']:
+            json_payload = {}
+            for key, value in x['_source'].items(): json_payload[key] = value
+            json_payload['_score'] = x['_score']
+            data_cards.append(json_payload)
+
         total_hits = 0
 
         try:
@@ -106,7 +130,7 @@ def get_results_data():
         except Exception as e:
             pass
 
-        return {"data": data_cards, "total_hits": total_hits}
+        return {"data": data_cards, "total_hits": total_hits, "scroll_id": scroll_id}
 
     except Exception as e:
         print("Error", e)
